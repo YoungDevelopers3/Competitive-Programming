f2a8385dced58bf8c22ecc5d02241fe372fd1a97
YoungDevelopers3 <62033851+YoungDevelopers3@users.noreply.github.com>
2024-11-19 17:13:22 +0530

    Create GPL.cpp

---
+++ GPL.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n=0;
+		int long long d=0;
+        string b="";
+        cin>>n;
+        cin>>b;
+      
+        d=stoll(b,nullptr,2)*1LL;
+        cout<<d<<endl;
+        
+    }
+}
