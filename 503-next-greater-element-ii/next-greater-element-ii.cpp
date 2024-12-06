ac201d4c0260d82c8c698b1881fdee994a6296fb
YoungDevelopers3 <62033851+YoungDevelopers3@users.noreply.github.com>
2024-12-06 23:48:42 +0530

    Time: 4 ms (72.71%) | Memory: 28.2 MB (17.26%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-    vector<int> result(n, -1); // Initialize result array with -1
-    stack<int> s; // Stack to store indices
+    vector<int> result(n, -1); 
+    stack<int> s; 
 
-    // Traverse the array twice to simulate circular behavior
+   
     for (int i = 0; i < 2 * n; i++) {
-        // Current element in the circular traversal
+     
         int num = nums[i % n];
 
-        // Resolve the next greater element for indices in the stack
+      
         while (!s.empty() && nums[s.top()] < num) {
-            result[s.top()] = num; // Update result for the top index
-            s.pop();               // Remove the resolved index
+            result[s.top()] = num; 
+            s.pop();               
         }
 
-        // Push the index if within the first traversal (to avoid duplicates)
+        
         if (i < n) {
             s.push(i);
         }
