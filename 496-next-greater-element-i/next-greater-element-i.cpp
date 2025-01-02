58e94c464b94f6c6413e55b09a9f57bd399ef089
YoungDevelopers3 <62033851+YoungDevelopers3@users.noreply.github.com>
2025-01-02 13:52:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (17.57%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,28 +3,21 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int>result;
         stack<int>s;
-        unordered_map<int, int> nextGreater;
-
-    // Traverse nums2 in reverse to find the next greater element for each number
-    for (int i = nums2.size() - 1; i >= 0; --i) {
-        // Pop elements from the stack that are less than or equal to nums2[i]
-        while (!s.empty() && s.top() <= nums2[i]) {
-            s.pop();
+        unordered_map<int,int>um;
+ 
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(!s.empty() && nums2[i]>=s.top()){
+                s.pop();
+            }
+            um[nums2[i]]=s.empty() ? -1:s.top(); 
+            s.push(nums2[i]);
+        }   
+        for(int num:nums1){
+            result.push_back(um[num]);
         }
 
-        // If the stack is empty, no greater element exists
-        nextGreater[nums2[i]] = s.empty() ? -1 : s.top();
+        return result;
 
-        // Push the current element onto the stack
-        s.push(nums2[i]);
-    }
 
-    // Build the result for nums1 using the nextGreater map
-    for (int num : nums1) {
-        result.push_back(nextGreater[num]);
-    }
-
-    return result;
-    
     }
 };
