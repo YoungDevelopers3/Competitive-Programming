d7bfae6a7afa9fb479fe2948823a2f47b4b39aed
YoungDevelopers3 <62033851+YoungDevelopers3@users.noreply.github.com>
2025-01-22 21:20:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remainder Evaluation/remainder-evaluation.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int findRemainder(int n1, int n2) {
+       return n1%n2;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int num1, num2;
+        cin >> num1 >> num2;
+        Solution ob;
+        int ans = ob.findRemainder(num1, num2);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
