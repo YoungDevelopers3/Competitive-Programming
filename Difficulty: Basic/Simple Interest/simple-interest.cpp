4d9eabd9a1b7dd8db30bec0855e12d118d6702ba
YoungDevelopers3 <62033851+YoungDevelopers3@users.noreply.github.com>
2025-01-29 20:41:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Simple Interest/simple-interest.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    double simpleInterest(int P, int R, int T) {
+      double si=double(P*R*T)/100;
+        return si;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int P, R, T;
+        cin >> P >> R >> T;
+        Solution ob;
+        cout << fixed << setprecision(2);
+        cout << ob.simpleInterest(P, R, T) << "\n";
+    
+cout << "~" << "\n";
+}
+}
+
+// } Driver Code Ends
