de366138100122c69bedd485fc8b13105c68e55a
YoungDevelopers3 <62033851+YoungDevelopers3@users.noreply.github.com>
2025-01-30 22:38:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Display longest name/display-longest-name.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string longest(vector<string>& arr) {
+        string max=arr[0];
+        for(int i=1;i<arr.size();i++){
+            if(max.size()<arr[i].size()){
+                max=arr[i];
+            }
+        }
+        return max;
+        
+        
+        
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<string> names;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        string str;
+        while (ss1 >> str) {
+            names.push_back(str);
+        }
+        Solution obj;
+        string res = obj.longest(names);
+        cout << res << "\n~\n";
+    }
+}
+
+// } Driver Code Ends
