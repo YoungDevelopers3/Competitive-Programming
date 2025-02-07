ed135490f14d906d7a1e6d2255c48230a0fdc5e3
YoungDevelopers3 <62033851+YoungDevelopers3@users.noreply.github.com>
2025-02-07 20:09:53 +0530

    Time: 26 ms (6.00%) | Memory: 39.6 MB (6.56%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
+
+    bool isPossible(vector<int>piles,int h,int min_ban){
+    double total_hours=0;
+    for(int pile:piles){
+        total_hours=total_hours+ceil((double)(pile)/(double)(min_ban));
+    }
+    
+    if(total_hours<=h){
+        return true;
+    }
+    else{
+        return false;
+    }
+        
+        
+    }
+
+
     int minEatingSpeed(vector<int>& piles, int h) {
-        sort(piles.begin(),piles.end());
-        int n=piles.size();
-        int high=piles[n-1];
-        int low=1;
-        int ans=0;
+    sort(piles.begin(),piles.end());
+    int n=piles.size();
+    int low=1;
+    int high=piles[n-1];
+    int ans=0;
         while(low<=high){
-             
-            int mid=low+(high-low)/2;
-            long long int hours =0;
-            for(int pile:piles){
-                // hours=hours+ceil((double)pile/mid);
-                if(pile%mid==0)
-                hours+= pile/mid;
-                else
-                {hours+= pile/mid;
-                hours++;
-                }
-            }
-         
-
-            if(hours<=h){
-                ans=mid;
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-
-        }  
-        return ans;
-         
+        int mid=low+(high-low)/2;
+    
+        if(isPossible(piles,h,mid)){ 
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    
+      }
+      return ans;
+ 
     }
 };
