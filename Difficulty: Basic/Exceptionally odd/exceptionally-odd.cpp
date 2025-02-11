94dc068e333fa3318d530c79bb5f6f5fb9f2672f
YoungDevelopers3 <62033851+YoungDevelopers3@users.noreply.github.com>
2025-02-11 14:39:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Exceptionally odd/exceptionally-odd.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int getOddOccurrence(int arr[], int n) {
+        unordered_map<int,int>freq;
+        
+        for(int i=0;i<n;i++){
+            freq[arr[i]]++;
+        }
+        
+        for(auto p:freq){
+            if(p.second %2 !=0){
+                return p.first;
+            }
+        }
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getOddOccurrence(arr, n);
+        cout << ans << "\n";
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+
+
+// } Driver Code Ends
