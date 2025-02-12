ea425a19c5ca990477fbfb7356a2dd81663d6585
YoungDevelopers3 <62033851+YoungDevelopers3@users.noreply.github.com>
2025-02-12 23:09:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Average of Prefix/average-of-prefix.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    vector<int> prefixAvg(vector<int> &arr) {
+        
+        vector<int>result;
+        int s=0;
+        for(int i=0;i<arr.size();i++){
+            s=s+arr[i];
+            int ans=0;
+            ans=s/(i+1);
+            result.push_back(ans);
+        }
+        return result;
+        
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream s1(input);
+        int num;
+        while (s1 >> num) {
+            arr.push_back(num);
+        }
+        Solution ob;
+        vector<int> ans = ob.prefixAvg(arr);
+
+        for (auto x : ans) {
+            cout << x << " ";
+        }
+        cout << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
