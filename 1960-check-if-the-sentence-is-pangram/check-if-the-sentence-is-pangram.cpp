949641ccc7a3882a4072271f9098d6cc807d10e9
YoungDevelopers3 <62033851+YoungDevelopers3@users.noreply.github.com>
2025-04-04 14:58:58 -0400

    Time: 0 ms (100.00%) | Memory: 9 MB (39.24%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-
-
        set<char>alpha;
-for(char ch:sentence){
-    alpha.insert(ch);
-}
-if(alpha.size()==26){
-    return true;
-}
-else{
-    return false;
-}
-
-
-
-
-
-        
+       for(char ch:sentence){
+          alpha.insert(ch);
+       }
+       if(alpha.size()==26){
+        return true;
+       }
+       return false;
     }
 };
