4ee3e8fe6f00e146422d2175a6987e7d0f98a8b9
YoungDevelopers3 <62033851+YoungDevelopers3@users.noreply.github.com>
2025-04-05 13:36:45 -0400

    Time: 0 ms (100.00%) | Memory: 8.6 MB (12.43%) - LeetSync

--- 2752-sum-multiples/sum-multiples.cpp
+++ 2752-sum-multiples/sum-multiples.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int sumOfMultiples(int n) {
-        int sum=0;
+        int s=0;
         for(int i=1;i<=n;i++){
-            if(i%3==0||i%5==0||i%7==0){
-               sum=sum+i;
-            }
+             if(i%3==0||i%5==0||i%7==0){
+                s=s+i;
+             }
         }
-        return sum;
+        return s;
     }
 };
