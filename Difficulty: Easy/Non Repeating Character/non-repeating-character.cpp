0d478338657a6a3df2a1b161f69d7cea52b8fed0
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-04-25 18:17:18 -0400

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    char nonRepeatingChar(string &s) {
+        unordered_map<char,int>freq;
+        for(char ch:s){
+            freq[ch]++;
+        }
+        for(char ch:s){
+            if(freq[ch]==1){
+                return ch;
+            }
+        }
+        return '$';
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+
+        string S;
+        cin >> S;
+        Solution obj;
+        char ans = obj.nonRepeatingChar(S);
+
+        if (ans != '$')
+            cout << ans;
+        else
+            cout << "-1";
+
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
