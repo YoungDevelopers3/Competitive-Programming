d5cc71b1c294408abf4113051170b3020aae700f
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-05-24 21:48:25 -0400

    Time: 0 ms (100.00%) | Memory: 31.4 MB (99.72%) - LeetSync

---
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        reverse(num.begin(),num.end());
+        int i=0;
+        
+        while(k>0){
+            int d=k%10;
+            k=k/10;
+            if(i<num.size()){
+                num[i]+=d;
+            }
+            else{
+                num.push_back(d);
+            }
+            int carry=0;
+            carry=num[i]/10;
+            num[i]=num[i]%10;
+            k+=carry;
+            i++;
+
+        }
+        reverse(num.begin(),num.end());
+        return num;
+    }
+};
