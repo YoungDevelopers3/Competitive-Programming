9b9175b60c08019b830ff1dbc3d83a08275b0c83
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-06-05 20:30:08 -0400

    Time: 8 ms (8.73%) | Memory: 9.6 MB (74.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,12 +3,8 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        if(s.size()==t.size()){
-            for(int i=0;i<s.size();i++){
-                if(s[i]!=t[i]){
-                    return false;
-                }
-            }
+        if(s.size()==t.size() && s==t){
+            
             return true;
         }
         return false;
