0676619b598d051ce5759f1e50ea1ab417918add
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-06-23 16:18:42 -0400

    Time: 0 ms (100.00%) | Memory: 14.4 MB (29.28%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        set<int>st;
-        for(int i:nums){
-            st.insert(i);
-        }
-        nums.clear();
-        for(auto i:st){
-            nums.push_back(i);
-        }
-         int n=nums.size();
-        if(n<3){
-           return nums[n-1];
-        }
-        return nums[n-3];
+        set<int>st(nums.begin(),nums.end());
+       if(st.size()<3){
+           return *st.rbegin();
+       }
+       auto it=st.rbegin();
+       advance(it,2);
+       return *it;
     }
 };
