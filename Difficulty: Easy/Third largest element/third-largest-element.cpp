9f9185dfc245f94188677f4899d6554cbf054893
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-06-23 16:20:49 -0400

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Third largest element/third-largest-element.cpp
+++ Difficulty: Easy/Third largest element/third-largest-element.cpp
@@ -1,46 +1,12 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     int thirdLargest(vector<int> &arr) {
-       if(arr.size()<3){
-           return -1;
-       }
-       else{
-           sort(arr.begin(),arr.end());
-           reverse(arr.begin(),arr.end());
-           return arr[2];
-       }
+      if(arr.size()<3){
+          return -1;
+      }
+      sort(arr.begin(),arr.end());
+      int n=arr.size();
+      return arr[n-3];
+        
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        cout << ob.thirdLargest(arr) << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
