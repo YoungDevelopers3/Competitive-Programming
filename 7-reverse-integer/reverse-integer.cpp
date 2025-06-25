c38c0426d8b31826b6f2790d9879b0fb0b1cc150
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-06-25 03:31:29 -0400

    Time: 7 ms (6.12%) | Memory: 9.5 MB (5.06%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        long long ans=0;
+        if(x>0){
+            string num=to_string(x);
+            std::reverse(num.begin(),num.end());
+            ans=stoll(num);
+        }
+        else{
+            if(x==INT_MIN){
+                return 0;
+            }
+            x=x*-1;
+            string num=to_string(x);
+            std::reverse(num.begin(),num.end());
+            ans=stoll(num);
+            if(ans>INT_MAX){
+                return 0;
+            }
+            ans=ans*-1;
+        }
+        if(ans<INT_MAX && ans>INT_MIN){
+            return ans;
+        }
+        return 0;
+    }
+};
