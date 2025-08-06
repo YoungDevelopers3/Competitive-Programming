8b99e232bd7cef052d94b5c627cf56af7d341174
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-08-06 16:25:01 -0400

    Time: 7 ms (88.88%) | Memory: 42.6 MB (99.76%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.cpp
+++ 924-fair-candy-swap/fair-candy-swap.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
-        int sumA = accumulate(aliceSizes.begin(), aliceSizes.end(), 0);
-    int sumB = accumulate(bobSizes.begin(), bobSizes.end(), 0);
-
-    int diff = (sumA - sumB) / 2;
-
-    
-    sort(bobSizes.begin(), bobSizes.end());
-
-    for (int a : aliceSizes) {
-        int b = a - diff;
-        if (binary_search(bobSizes.begin(), bobSizes.end(), b)) {
-            return {a, b};
+        long long int sum_a=accumulate(aliceSizes.begin(),aliceSizes.end(),0);
+        long long int sum_b=accumulate(bobSizes.begin(),bobSizes.end(),0);
+        int diff=(sum_a-sum_b)/2;
+        sort(aliceSizes.begin(),aliceSizes.end());
+        for(int b:bobSizes){
+            int a=diff+b;
+            if(binary_search(aliceSizes.begin(),aliceSizes.end(),a)){
+                return {a,b};
+            }
         }
-    }
-
-    return {}; 
-
-
-
+        return {};
 
     }
 };
