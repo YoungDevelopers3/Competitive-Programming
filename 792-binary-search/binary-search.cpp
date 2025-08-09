9003a4dbcf22976a888964b47cc1fa3ad86ecfd6
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-08-09 18:53:09 -0400

    Time: 0 ms (100.00%) | Memory: 31.5 MB (9.50%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -8,11 +8,11 @@ public:
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid]<target) {
-                 low=mid+1;
+            else if(nums[mid]>target){
+                high=mid-1;
             }
             else{
-                high=mid-1;
+                low=mid+1;
             }
         }
         return -1;
