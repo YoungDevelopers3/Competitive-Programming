7145755fa2f47b9f534c777259d41a41c78ba07c
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-08-16 20:53:12 -0400

    Time: 3 ms (31.79%) | Memory: 12.9 MB (25.73%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> allSubsets;
-
-    void generate(vector<int> &subset,int i, vector<int> &nums){
-        if(i==nums.size()){
-            allSubsets.push_back(subset);
+    vector<vector<int>>all_subsets;
+    void generate(vector<int> &subset,int i,vector<int> &nums){
+          if(i==nums.size()){
+            all_subsets.push_back(subset);
             return;
-        }
-        //i th element not in subset
-        generate(subset,i+1,nums);
-
-        //i th element in subset
-        subset.push_back(nums[i]);
-        generate(subset,i+1,nums);
-        subset.pop_back();
-
+          }
+          generate(subset,i+1,nums);
+          subset.push_back(nums[i]);
+          generate(subset,i+1,nums);
+          subset.pop_back();
     }
-
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>empty;
-        generate(empty,0,nums);
-        return allSubsets;
-        
+        vector<int>subset;
+        int i=0;
+        generate(subset,i,nums);        
+        return all_subsets;
+
     }
 };
