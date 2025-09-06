08ba5e8c8ebf27e8a9b4cfdb30890126597988f6
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-09-06 00:37:39 -0400

    Time: 0 ms (100.00%) | Memory: 9.5 MB (8.17%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,46 +1,53 @@
 class Solution {
 public:
     int myAtoi(string s) {
-       while (!s.empty() && s[0] == ' ') {
-        s.erase(0, 1);
-    }
-    int sign=1;
-    if(!s.empty() &&(s[0]=='-'||s[0]=='+')){
-        if(s[0]=='-'){
+        while(!s.empty() && s[0]==' '){
+            s.erase(0,1);
+        }
+        int sign=1;
+        if(!s.empty() && (s[0]=='-' || s[0]=='+')){
+          if(s[0]=='-'){
             sign=-1;
+            s.erase(0,1);
+          }
+          else{
+            sign =1;
+            s.erase(0,1);
+          }
+                
         }
-        if(s[0]=='+'){
-            sign=1;
+         
+        string temp="";
+        if(!s.empty()){
+            for(char ch:s){
+                if(!isdigit(ch)){
+                    break;
+                }
+                else{
+                    temp=temp+ch;
+                }
+            }
+           
         }
-        s.erase(0,1);
-    }
-
-       string n1="";
-       for(char ch:s){
-        if(!isdigit(ch)){
-              break;
+        long long ans=0;
+        if(temp.empty()){
+            return ans;
         }
         else{
-            n1=n1+ch;
+            for(int i=0;i<temp.size();i++){
+                ans=ans*10+temp[i]-'0';
+                if(sign ==-1 && sign*ans<INT_MIN){
+                    return INT_MIN;
+                  }
+                if(sign ==1 && sign*ans>INT_MAX){
+                     return INT_MAX;
+                   }
+            }
         }
 
-       }
-      if(n1.empty()){
-        return 0;
-       }
-       long long ans=0;
-       for(char ch:n1){
-        ans=ans*10+ch-'0';
-        if(sign==-1 && sign*ans<INT_MIN){
-            return INT_MIN;
-        }
-        if(sign==1 && sign*ans>INT_MAX){
-            return INT_MAX;
-        }
-       }
-       ans=ans*sign;
-       return ans;
-       
+        return ans*sign;
         
+
+
     }
 };
