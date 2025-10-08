8172ded03f6bc203a00de73ed364f80488bf3959
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-10-08 18:18:37 -0400

    Time: 3 ms (8.61%) | Memory: 10.1 MB (5.04%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,12 +3,12 @@ public:
     bool isValid(string s) {
         stack<int>st;
         for(char ch:s){
-            if(ch=='('|| ch=='{'|| ch=='['){
+            if(ch=='('|| ch=='{'||ch=='['){
                 st.push(ch);
             }
             else{
                 if(!st.empty()){
-                    if(ch==')' && st.top()=='('|| ch=='}' && st.top()=='{' || ch==']' && st.top()=='['){
+                    if(ch==')' && st.top()=='(' ||ch=='}' && st.top()=='{'|| ch==']'&& st.top()=='['){
                         st.pop();
                     }
                     else{
