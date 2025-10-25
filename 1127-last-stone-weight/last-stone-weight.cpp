27f7a04782feb88a48ba98332577b7c48320df73
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2025-10-25 18:23:33 -0400

    Time: 0 ms (100.00%) | Memory: 9.9 MB (43.91%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(int i:stones){
+            pq.push(i);
+        }
+
+        while(pq.size()>1){
+            int a=pq.top();
+            pq.pop();
+            int b=pq.top();
+            pq.pop();
+
+            if(a==b){
+                pq.push(0);
+            }
+            else{
+                pq.push(abs(a-b));
+            }
+        }
+        return pq.top();
+
+    }
+};
