894a7ffc3f09a506e8e7ab1859b86a56e889d2df
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-01-30 14:25:45 -0500

    Time: 11 ms (39.48%) | Memory: 15.9 MB (7.72%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numMap;
-        int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.count(complement)) {
-                return {numMap[complement], i};
+        vector<int>ans;
+        unordered_map<int,int>freq;
+        for(int i=0;i<nums.size();i++){
+            int x=target-nums[i];
+            if(freq.count(x)){
+                ans.push_back(freq[x]);
+                ans.push_back(i);
             }
-            numMap[nums[i]] = i;
+            freq[nums[i]]=i;
         }
-
-        return {}; 
+        return ans;
     }
 };
