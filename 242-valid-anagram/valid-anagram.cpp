7124cbd31a6636288c65dec570b29fd3cf34f255
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-01-30 13:20:36 -0500

    Time: 5 ms (26.34%) | Memory: 9.6 MB (74.08%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,8 +3,7 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        if(s.size()==t.size() && s==t){
-            
+        if(s==t){
             return true;
         }
         return false;
