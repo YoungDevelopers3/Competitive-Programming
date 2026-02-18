a5e06fc5620aff7717391767d555892796753716
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-02-18 16:52:29 -0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BST Insert/bst-insert.cpp
@@ -0,0 +1,30 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* insert(Node* root, int key) {
+        if(root==NULL){
+            return new Node(key);
+        }
+        
+        if(key<root->data){
+            root->left=insert(root->left,key);
+        }
+        else{
+            root->right=insert(root->right,key);
+        }
+        return root;
+    }
+};
