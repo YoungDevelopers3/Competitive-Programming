856ae2431eec322e18a72420e511a8192e2eeefd
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-03-18 12:19:01 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (13.45%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -15,6 +15,6 @@ public:
         if(root==NULL){
             return 0;
         }
-        return max(maxDepth(root->left),maxDepth(root->right))+1;
+       return 1+max(maxDepth(root->right),maxDepth(root->left));
     }
 };
