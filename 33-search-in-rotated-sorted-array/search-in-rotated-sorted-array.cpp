da4c03a9b0c72f5185263112d796a82082b6e9bf
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-03-25 14:46:15 -0400

    Time: 0 ms (100.00%) | Memory: 15.1 MB (67.06%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low=0;
-        int high=nums.size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && nums[mid]>=target){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
-            }
-            else{
-                if(nums[mid]<=target && nums[high]>=target){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
+        int l = 0;
+        int r = nums.size() - 1;
+
+        // STEP 1: find pivot
+        while(l < r){
+            int mid = l + (r - l)/2;
+            if(nums[mid] > nums[r]){
+                l = mid + 1;
+            } else {
+                r = mid;
             }
         }
+
+        int pivot = l;
+        int n = nums.size();
+
+        // STEP 2: choose correct half
+        if(target >= nums[pivot] && target <= nums[n-1]){
+            l = pivot;
+            r = n - 1;
+        } else {
+            l = 0;
+            r = pivot - 1;
+        }
+
+        // STEP 3: normal binary search
+        while(l <= r){
+            int mid = l + (r - l)/2;
+
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) r = mid - 1;
+            else l = mid + 1;
+        }
+
         return -1;
     }
 };
