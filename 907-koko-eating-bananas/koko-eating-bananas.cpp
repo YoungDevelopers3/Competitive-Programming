4f9aef90cfe7930d1f013220f021cd216cf8a97a
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-05-07 01:38:13 -0400

    Time: 39 ms (5.35%) | Memory: 23 MB (44.76%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
-
-    bool isPossible(vector<int>piles,int h,int min_ban){
-    double total_hours=0;
-    for(int pile:piles){
-        total_hours=total_hours+ceil((double)(pile)/(double)(min_ban));
+    long long caltotalhr(vector<int>& piles,int hr){
+       long long int total=0;
+        for(int i=0;i<piles.size();i++){
+            total+=ceil((double)piles[i]/(double)hr);
+        }
+        return total;
     }
-    
-    if(total_hours<=h){
-        return true;
-    }
-    else{
-        return false;
-    }
-        
-        
-    }
-
-
     int minEatingSpeed(vector<int>& piles, int h) {
-    sort(piles.begin(),piles.end());
-    int n=piles.size();
-    int low=1;
-    int high=piles[n-1];
-    int ans=0;
+        int n=piles.size();
+        int low=1;
+        sort(piles.begin(),piles.end());
+      int high=piles[n-1];
+        int ans=0;
         while(low<=high){
-        int mid=low+(high-low)/2;
-    
-        if(isPossible(piles,h,mid)){ 
-            ans=mid;
-            high=mid-1;
+            int mid=low+((high-low)/2);
+            long long int temp=caltotalhr(piles,mid);
+            if(temp<=h){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
-        else{
-            low=mid+1;
-        }
-    
-      }
-      return ans;
- 
+        return ans;
+        
     }
 };
