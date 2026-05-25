9c4fa06ae6d3f64dca3e9dfdeedda6faa64c526d
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-05-25 16:32:56 -0400

    Time: 0 ms (100.00%) | Memory: 19.6 MB (61.36%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    void dfs(int city,vector<vector<int>>& isConnected,vector<bool> &visited){
-        visited[city]=true;
-        for (int j = 0; j < isConnected.size(); j++) {
-            if (isConnected[city][j] == 1 && !visited[j]) {
-                dfs(j, isConnected, visited);
+
+    void dfs(int start,vector<vector<int>>& isConnected, vector<int>& visited){
+        visited[start]=true;
+        for(int i=0;i<isConnected.size();i++){
+            if(!visited[i] && isConnected[start][i]==1){
+                dfs(i,isConnected,visited);
             }
         }
     }
+
+
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int V=isConnected.size();
-        vector<bool>visited(V,false);
+        int v=isConnected.size();
+        vector<int>visited(v,false);
         int provinces =0;
-        for(int i=0;i<V;i++){
+        for(int i=0;i<v;i++){
             if(!visited[i]){
                 provinces++;
                 dfs(i,isConnected,visited);
             }
         }
         return provinces;
+
+
     }
 };
