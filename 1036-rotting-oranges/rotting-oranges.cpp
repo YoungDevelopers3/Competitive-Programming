013912285a6ecd14cc6f0ad9aadf42e271e9ad63
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-05-26 13:52:59 -0400

    Time: 1 ms (37.95%) | Memory: 17 MB (45.64%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,50 +3,45 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int ans=0;
-
-        vector<vector<bool>> visited(n,vector<bool>(m,false));
+        int time=0;
         queue<pair<pair<int,int>,int>>q;
-
+        vector<vector<int>>visited(n,vector<int>(m,false));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j}, 0});
                     visited[i][j]=true;
+                    q.push({{i,j},0});
                 }
             }
         }
-    
-        while(q.size()>0){
+
+        while(!q.empty()){
             int i=q.front().first.first;
             int j=q.front().first.second;
-            int time=q.front().second;
+            time=q.front().second;
             q.pop();
 
-            ans=max(ans,time);
-            //top
-            if(i-1>=0 && !visited[i-1][j] && grid[i-1][j]==1){
-                q.push({{i-1,j},time+1});
-                visited[i-1][j]=true;
+            if(i-1>=0 && grid[i-1][j]==1 && !visited[i-1][j]){
+                 q.push({{i-1,j},time+1});
+                 visited[i-1][j]=true;
             }
-            //right
-            if(j+1<m && !visited[i][j+1] && grid[i][j+1]==1){
-                q.push({{i,j+1},time+1});
-                visited[i][j+1]=true;
-            }
-            //bottom
-            if(i+1<n && !visited[i+1][j] && grid[i+1][j]==1){
+            if(i+1<n && grid[i+1][j]==1 && !visited[i+1][j]){
                 q.push({{i+1,j},time+1});
                 visited[i+1][j]=true;
             }
-            //left
-            if(j-1>=0 && !visited[i][j-1] && grid[i][j-1]==1){
+            if(j-1>=0 && grid[i][j-1]==1 && !visited[i][j-1]){
                 q.push({{i,j-1},time+1});
-                visited[i][j-1]=true;
+                    visited[i][j-1]=true;
+                
             }
+            if(j+1<m && grid[i][j+1]==1 && !visited[i][j+1]){
+                q.push({{i,j+1},time+1});
+                visited[i][j+1]=true;
+            } 
+            
+
         }
 
-        //check for fresh orange
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1 && !visited[i][j]){
@@ -55,7 +50,8 @@ public:
             }
         }
 
-        return ans;
+        return time;
+
 
 
     }
