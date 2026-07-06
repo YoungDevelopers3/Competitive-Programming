eec84804d1fd5cd78cbc128f7a6fe15d11073ea5
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-07-06 17:43:14 -0400

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -1,40 +1,31 @@
 class Solution {
   public:
-  
-  int knapSackRecur(int i, int capacity, vector<int> &val, 
-                  vector<int> &wt, vector<vector<int>> &memo) {
-    if (i == val.size())
-        return 0;
-
-    // If value is memoized.
-    if (memo[i][capacity] != -1)
-        return memo[i][capacity];
-
-    // Consider current item only if
-    // its weight is less than equal
-    // to maximum weight.
-    int take = 0;
-    if (wt[i] <= capacity) {
-        take = val[i] + knapSackRecur(i, capacity - wt[i],
-                                      val, wt, memo);
+    int calc(int n,vector<int>& val,vector<int>& wt,int capacity, vector<vector<int>>& dp){
+        if(n==0){
+            return 0;
+        }
+        if(dp[n][capacity]!=-1){
+            return dp[n][capacity];
+        }
+        int not_take=0;
+        not_take=calc(n-1,val,wt,capacity,dp);
+        
+        int take=0;
+        if(wt[n-1]<=capacity){
+            take=val[n-1]+calc(n,val,wt,capacity-wt[n-1],dp);
+        }
+        
+        return dp[n][capacity]=max(take,not_take);
+        
+        
+        
     }
-                  
-
-    // Skip the current item
-    int noTake = knapSackRecur(i + 1, capacity, val, wt, memo);
-
-    // store maximum of the two and return it.
-    return memo[i][capacity] = max(take, noTake);
-                  }
-
   
     int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
         // code here
-        
-          vector<vector<int>> memo(val.size(), vector<int>(capacity + 1, -1));
-
-    return knapSackRecur(0, capacity, val, wt, memo);
-        
-        
+        int n=val.size();
+        vector<vector<int>>dp(n+1,vector<int>(capacity+1,-1));
+        return calc(n,val,wt,capacity,dp);
+    
     }
 };
