52d3caecfe9d446aa384805eccdf28da2a89d343
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-08-13 03:36:44 -0400

    Time: 37 ms (5.39%) | Memory: 31.3 MB (5.61%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,56 +1,65 @@
 class Solution {
 public:
 
-   int calc(int i,int j,string &str1,string &str2,vector<vector<int>>& dp){
-
-         if(i==0||j==0){
+    int calc(vector<vector<int>> &dp,string &str1,string &str2,int i,int j){
+        if(i==0 ||j==0){
             return 0;
-         }
-
-         if(dp[i][j]!=-1){
+        }
+        if(dp[i][j]!=-1){
             return dp[i][j];
-         }
+        }
+        if(str1[i-1]==str2[j-1]){
+            dp[i][j]=1+calc(dp,str1,str2,i-1,j-1);
+        }
+        else{
+            dp[i][j]=max(calc(dp,str1,str2,i-1,j),calc(dp,str1,str2,i,j-1));
+        }
 
-         if(str1[i-1]==str2[j-1]){
-            dp[i][j]=1+calc(i-1,j-1,str1,str2,dp);
-         }
-         else{
-            dp[i][j]=max(calc(i-1,j,str1,str2,dp),calc(i,j-1,str1,str2,dp));
-         }
-        
         return dp[i][j];
 
-   }
+    }
+    
+
+
     string shortestCommonSupersequence(string str1, string str2) {
         int n=str1.size();
         int m=str2.size();
-
-       vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-
-       calc(n,m,str1,str2,dp);
-
-       string result;
-        int i = n, j = m;
-        while (i > 0 && j > 0) {
-            if (str1[i-1] == str2[j-1]) {
-                result.push_back(str1[i-1]);
-                i--; j--;
-            }
-            else if (calc(i-1, j, str1, str2, dp) >= calc(i, j-1, str1, str2, dp)) {
+        int i=n;
+        int j=m;
+        vector<vector<int>>dp(n+1,vector<int>((m+1),-1));
+        calc(dp,str1,str2,n,m);
+        vector<char>result;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
                 result.push_back(str1[i-1]);
                 i--;
-            }
-            else {
-                result.push_back(str2[j-1]);
                 j--;
+            }
+            else if(calc(dp,str1,str2,i-1,j)>=calc(dp,str1,str2,i,j-1)){
+                 result.push_back(str1[i-1]);
+                 i--;      
+            }
+            else{
+                 result.push_back(str2[j-1]);
+                 j--;      
             }
         }
 
-        while (i > 0) { result.push_back(str1[i-1]); i--; }
-        while (j > 0) { result.push_back(str2[j-1]); j--; }
+        while(i>0){
+            result.push_back(str1[i-1]);
+            i--;
+        }
+        while(j>0){
+            result.push_back(str2[j-1]);
+            j--;
+        }
+        reverse(result.begin(),result.end());
+       string ans="";
+       for(char ch:result){
+         ans=ans+ch;
+       }
 
-        reverse(result.begin(), result.end());
-        return result;
+       return ans;
 
     }
 };
