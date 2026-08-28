5dcf46e071ac85ff4192d20dd98d8f42d2e98650
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-08-28 04:11:00 -0400

    Time: 0 ms (100.00%) | Memory: 52.2 MB (83.23%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int max_reach=0;
+        bool ans=true;
+        for(int i=0;i<n;i++){
+          
+           if(i>max_reach){
+              ans=false;
+              break;
+           }
+         max_reach=max(max_reach,i+nums[i]);
+        }
+        return ans;
+    }
+};
