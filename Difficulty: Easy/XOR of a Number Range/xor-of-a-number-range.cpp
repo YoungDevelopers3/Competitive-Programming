0faa44e8279b4d6399a0b7946f0a71a063e2031d
Silent_Shark <62033851+YoungDevelopers3@users.noreply.github.com>
2026-09-13 20:46:46 -0400

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/XOR of a Number Range/xor-of-a-number-range.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+  
+  
+    int XorRangeValues(int n){
+        if(n%4==0){
+            return n;
+        }
+        else if(n%4==1){
+            return 1;
+        }
+        else if(n%4==2){
+            return n+1;
+        }
+        else{
+            return 0;
+        }
+    }
+  
+  
+    int findXOR(int l, int r) {
+        // code here
+        return XorRangeValues(r) ^ XorRangeValues(l-1);
+    }
+};
